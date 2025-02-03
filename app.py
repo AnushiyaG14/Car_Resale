987f04ed5f376b68d5e72be87c3853739608c321
Anushiya G <anushiya2014@gmail.com>
2025-02-03 22:03:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the trained model
-with open('D:\streamlit\env\prj3\model.pkl', 'rb') as file:
+with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Streamlit app
